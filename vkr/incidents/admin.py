56d78d270c7b86f3023d2cbb689226180d465a91
Стрельников Даниил Юрьевич <dyustrelnikov@edu.hse.ru>
2024-04-26 22:00:04 +0300

    added admin panel

--- vkr/incidents/admin.py
+++ vkr/incidents/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Basis, Critery, Expert, Incident, Status, Strategy
+
+admin.site.register(Basis)
+admin.site.register(Critery)
+admin.site.register(Expert)
+admin.site.register(Incident)
+admin.site.register(Status)
+admin.site.register(Strategy)
