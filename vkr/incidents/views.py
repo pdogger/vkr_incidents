f928ca280d9f230d2249b01aec27c175f60adbc0
Стрельников Даниил Юрьевич <dyustrelnikov@edu.hse.ru>
2024-04-27 23:53:06 +0300

    added login page and auth logic

--- vkr/incidents/views.py
+++ vkr/incidents/views.py
@@ -1,38 +1,66 @@
-from django.shortcuts import render
-from django.http import Http404, HttpResponseRedirect
-from .models import Incident, Expert
-from django.views.decorators.csrf import csrf_exempt
-from .forms import IncidentCreateForm
 import datetime
-from django.forms import modelformset_factory
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.http import Http404, HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm
 
+from .models import Incident, Expert
+from .forms import IncidentCreateForm
+
+
+def signin(request):
+    if request.method == 'GET':
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
+    elif request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+
+                return redirect('index')
+
+        return render(request, 'login.html', {'form': form})
+
+
+@login_required(login_url='login')
 def index(request):
     return render(request, "base.html")
 
+
+@login_required(login_url='login')
 def incidents_list(request):
     incidents = Incident.objects.all()
     return render(request, "incidents.html", {
             'incidents': incidents})
 
+
+@login_required(login_url='login')
 def incident_create(request):
-    ExpertFormSet = modelformset_factory(Expert, fields=["user"], max_num=7, extra=7)
     if request.method == 'POST':
         form_incident = IncidentCreateForm(request.POST)
-        expert_formset = ExpertFormSet(request.POST)
-        if form_incident.is_valid() and expert_formset.is_valid():
+        if form_incident.is_valid():
             creator, _ = Expert.objects.get_or_create(user_id=request.user.id)
             incident = Incident.objects.create(name=form_incident.cleaned_data['name'],
                                                 description=form_incident.cleaned_data['description'],
                                                   creator_id=creator, status_id=1,
                                                     created_at=datetime.datetime.now())
             return HttpResponseRedirect(f"/incident?incident_id={incident.id}")
-    
+
     else:
         form_incident = IncidentCreateForm()
-        expert_formset = ExpertFormSet()
 
-    return render(request, "incident_create.html", {"form_incident": form_incident, "expert_formset": expert_formset})
+    return render(request, "incident_create.html", {"form_incident": form_incident})
 
+
+@login_required(login_url='login')
 def incident(request):
     if request.method == 'GET':
         incident_id = request.GET['incident_id']
