3b577ca9e5f119d21b505d9244df80f92084a7a7
Стрельников Даниил Юрьевич <dyustrelnikov@edu.hse.ru>
2024-05-01 00:29:01 +0300

    idk for what but customized admin panel

--- vkr/incidents/admin.py
+++ vkr/incidents/admin.py
@@ -31,16 +31,54 @@ class StrategyInline(admin.TabularInline):
     model = Strategy
     extra = 1
 
+class BasisAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'incident')
+    search_fields = ['name', 'description']
+
+
+class CriteriaAdmin(admin.ModelAdmin):
+    list_display = ('description', 'id')
+    search_fields = ['description']
+
+
+class ExpertAdmin(admin.ModelAdmin):
+    list_display = ('user', 'fullname', 'id')
+    search_fields = ['user__username']
+
+    def fullname(self, obj):
+        return obj.user.get_full_name()
+
+
+class IncidentCriteriaAdmin(admin.ModelAdmin):
+    list_display = ('incident', 'criteria', 'number')
+    search_fields = ['incident__name']
+
+
+class IncidentExpertAdmin(admin.ModelAdmin):
+    list_display = ('incident', 'expert', 'number')
+    search_fields = ['incident__name']
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+
+
+class StrategyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'incident')
+    search_fields = ['name', 'description']
+
 
 class IncidentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'creator', 'created_at', 'status')
+    search_fields = ['name', 'description']
     inlines = [BasisInline, CriteriaInline, ExpertInline, StrategyInline]
 
 
-admin.site.register(Basis)
-admin.site.register(IncidentCriteria)
-admin.site.register(IncidentExpert)
-admin.site.register(Strategy)
-admin.site.register(Criteria)
-admin.site.register(Expert)
+admin.site.register(Basis, BasisAdmin)
+admin.site.register(Criteria, CriteriaAdmin)
+admin.site.register(Expert, ExpertAdmin)
 admin.site.register(Incident, IncidentAdmin)
-admin.site.register(Status)
+admin.site.register(IncidentCriteria, IncidentCriteriaAdmin)
+admin.site.register(IncidentExpert, IncidentExpertAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Strategy, StrategyAdmin)
