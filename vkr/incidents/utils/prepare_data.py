6b8dd68cb2c0988aa828c0d3a3f51762a132bb2f
Стрельников Даниил Юрьевич <dyustrelnikov@edu.hse.ru>
2024-05-05 19:35:32 +0300

    fixed strategy queries spam

--- vkr/incidents/utils/prepare_data.py
+++ vkr/incidents/utils/prepare_data.py
@@ -5,15 +5,18 @@ def prepare_results(incident):
         return {}
     results = {}
 
-    for criteria in incident.results.keys():
-        incident.results[criteria] = {k: v for k, v in sorted(incident.results[criteria].items(), key=lambda item: item[1])}
+    strategies = Strategy.objects.filter(incident=incident).all()
 
     for criteria in incident.results.keys():
         results[criteria] = {}
-        for key in incident.results[criteria].keys():
-            results[criteria][key] = {
-                'value': incident.results[criteria][key],
-                'strategy_name': Strategy.objects.get(incident=incident, number=key[-1]).name
+        for strategy in strategies:
+            results[criteria][f"S{strategy.number}"] = {
+                'value': incident.results[criteria][f"S{strategy.number}"],
+                'strategy_name': strategy.name
             }
+
+    for criteria in results.keys():
+        results[criteria] = {k: v for k, v in sorted(results[criteria].items(),
+                                                     key=lambda item: item[1]['value'])}
 
     return results
--- vkr/vkr/settings.py
+++ vkr/vkr/settings.py
@@ -75,13 +75,17 @@ WSGI_APPLICATION = 'vkr.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'vkrdb',
+    #     'USER': 'vkrvkr',
+    #     'PASSWORD': 'vkrvkr',
+    #     'HOST': 'localhost',
+    #     'PORT': 5432,
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vkrdb',
-        'USER': 'vkrvkr',
-        'PASSWORD': 'vkrvkr',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
